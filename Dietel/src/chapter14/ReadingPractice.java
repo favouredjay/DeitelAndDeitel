b5c46571e7fdd515bd9cc5a941a609c40a099e41
favouredjay <judom.2011@gmail.com>
2021-07-19 10:13:24 +0100

    practice

---
+++ Dietel/src/chapter14/ReadingPractice.java
@@ -0,0 +1,20 @@
+package chapter14;
+
+public class ReadingPractice {
+    public static void main(String[] args) {
+
+
+        String s = "JOY is my darling";
+        char []charArray = new char[5];
+        System.out.println("the reversed s is:");
+        for (int i = s.length()-1; i>=0; i--){
+            System.out.print(s.charAt(i));
+        }
+        System.out.println();
+        s.getChars(0, 5, charArray, 0);
+        for (char character : charArray)
+            System.out.print(character);
+
+
+    }
+}
