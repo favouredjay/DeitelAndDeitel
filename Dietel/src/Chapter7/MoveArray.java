857238cd1a363da5a92dd1170ea1b1a620fca7ed
favouredjay <judom.2011@gmail.com>
2021-07-20 21:12:46 +0100

    move array

---
+++ Dietel/src/Chapter7/MoveArray.java
@@ -0,0 +1,15 @@
+package Chapter7;
+
+public class MoveArray {
+    public static void main(String[] args) {
+        int [] array = {8,9,3,4,5,6};
+        int value1 = array[0];
+        for (int i =1; i<array.length;i++){
+            array[i-1] = array[i];
+
+        }
+        array[4] = value1;
+        for(int i : array)
+        System.out.print(i + ",");
+    }
+}
