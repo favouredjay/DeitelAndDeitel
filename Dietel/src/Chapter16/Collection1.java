a29b35bce9cd36d743d97d7513ee618f10afdf1f
favouredjay <judom.2011@gmail.com>
2021-10-29 07:40:39 +0100

    chapter 16

--- Dietel/src/Chapter16/Collection1.java
+++ Dietel/src/Chapter16/Collection1.java
@@ -13,20 +13,43 @@ public class Collection1 {
         }
 
     }
+    private static void removeList(List<String> list, int start, int end){
+        list.subList(start, end).clear();
+
+    }
 
     public static void main(String[] args) {
 
         String []colors = {"BLUE", "RED", "ORANGE", "GREEN"  };
-        List<String> list = new ArrayList<>();
+        List<String> list = new LinkedList<>();
 //        list.addAll(Arrays.asList(colors));
-        list.addAll(Arrays.asList(colors));
+        for(String color: colors)
+        list.add(color);
 
+        String [] colors2 = {"teal", "BROWN"};
+        List <String> list1 = new LinkedList<>();
+        for (String color: colors2){
+            list1.add(color);
+        }
+        list1.addAll(list);
+
+        Collections.sort(list1);
+        System.out.println("Combination of the two lists is: " + list1);
         String [] removeColors = {"GREEN", "RED"};
         List<String> removeList = new ArrayList<>();
         for(String removeColor: removeColors)
         removeList.add(removeColor);
 
+
+        ListIterator <String> iterator = list1.listIterator(list1.size());
+        System.out.println("backwards:");
+        while(iterator.hasPrevious()){
+
+        System.out.print(iterator.previous() + ", ");}
         removeItem(list, removeList);
+        removeList(list1, 1,5);
+        System.out.println();
+        System.out.println("items left are "+ list1);
 
         for(String color: list)
             System.out.println(color);
