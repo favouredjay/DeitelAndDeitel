df6a4024361df6a98cdab9ad4312084c49574bee
favouredjay <judom.2011@gmail.com>
2021-10-29 07:42:22 +0100

    Extras

--- Dietel/src/Extras/Check.java
+++ Dietel/src/Extras/Check.java
@@ -1,12 +1,25 @@
 package Extras;
 
-import java.util.stream.IntStream;
-
 public class Check {
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
+//
+//       IntStream value = IntStream.rangeClosed(1, 10)
+//                .map((int x) -> {return x + 2;});
+//        System.out.println(value);
+//    }
 
-       IntStream value = IntStream.rangeClosed(1, 10)
-                .map((int x) -> {return x + 2;});
-        System.out.println(value);
+    static String  joinWords(String [] word){
+        String sentence = "";
+        for(String w : word){
+            sentence += w;
+        }
+        return sentence;
+
+    }
+
+    public static void main(String[] args) {
+        String []word = new String[]{"a", "b", "c", "d", "e"};
+        System.out.print(joinWords(word));
+
     }
 }
