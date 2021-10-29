ea1a6addf13bce9f1b3ae0129333b512b34482cf
favouredjay <judom.2011@gmail.com>
2021-10-29 07:43:05 +0100

    Extras

--- Dietel/src/Extras/IsUnique.java
+++ Dietel/src/Extras/IsUnique.java
@@ -1,2 +1,37 @@
-package Extras;public class IsUnique {
+package Extras;
+
+public class IsUnique {
+    boolean isUnique = false;
+
+    public static boolean unique(String array) {
+        for (int i = 0; i < array.length(); i++) {
+            for (int j = i + 1; j < array.length(); j++) {
+                if (array.charAt(i) == array.charAt(j))
+                    return false;
+            }
+        }return true;
+    }
+
+    public static boolean uniqueArray(int [] array){
+        for (int i = 0; i<array.length; i++){
+            for(int j = i+1; j < array.length; j++){
+                if (array[i] == array[j])
+                    return false;
+
+            }
+        }return true;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        int [] array = {1,2,3,4,5};
+        System.out.println(unique("ayamatanga"));
+        System.out.println(unique("love"));
+        System.out.println(unique("pinkish"));
+        System.out.println();
+        System.out.println(uniqueArray(new int[]{2, 3, 2, 4, 5}));
+        System.out.println(uniqueArray(new int[]{1, 2, 3, 4, 5}));
+    }
 }
