2294342915e39a7db1942cc41c31d1e098017f59
favouredjay <judom.2011@gmail.com>
2021-10-29 07:36:14 +0100

    chapter 16

--- Dietel/src/Chapter16/Stack.java
+++ Dietel/src/Chapter16/Stack.java
@@ -1,2 +1,26 @@
-package Chapter16;public class Stack {
+package Chapter16;
+
+public class Stack {
+
+    public static void printStack(java.util.Stack<Number> stack){
+
+        System.out.println(stack.isEmpty()? "stack is empty" : "Stack contains " + stack);
+    }
+
+    public static void main(String[] args) {
+        java.util.Stack<Number> stack= new java.util.Stack<>();
+        stack.push(10L);
+        stack.push(123);
+        stack.push(2);
+        stack.add(0, 8);
+        printStack(stack);
+        System.out.println(stack.peek());
+        stack.pop();
+        printStack(stack);
+        stack.pop();
+        printStack(stack);
+        stack.pop();
+        stack.remove(0);
+        printStack(stack);
+    }
 }
