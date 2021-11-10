ccfa65138a7383ca962aba7e52cdfd26277464a0
favouredjay <judom.2011@gmail.com>
2021-11-10 14:49:02 +0000

    practice

---
+++ Dietel/src/chapter14/RegexSubstitution.java
@@ -0,0 +1,8 @@
+package chapter14;
+
+public class RegexSubstitution {
+    public static void main(String[] args) {
+        String firstWord = "Java is a cool language";
+        String secondWord = "Java is a programming language";
+    }
+}
